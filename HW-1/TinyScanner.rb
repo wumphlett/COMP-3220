90c26d86debc0791fb4fcbb21b990ca27e7eb29c
Will Humphlett <will@humphlett.net>
2021-01-30 13:04:47 -0600

    Change tabs to spaces

--- HW-1/TinyScanner.rb
+++ HW-1/TinyScanner.rb
@@ -25,55 +25,55 @@ class Scanner
 # Constructor - Is passed a file to scan and outputs a token
 #               each time nextToken() is invoked.
 #   @c        - A one character lookahead 
-	def initialize(filename)
-		# Need to modify this code so that the program
-		# doesn't abend if it can't open the file but rather
+    def initialize(filename)
+        # Need to modify this code so that the program
+        # doesn't abend if it can't open the file but rather
         # displays an informative message
         if (File.file?(filename))
             @f = File.open(filename,'r:utf-8')
         else
             raise StandardError, "Specified tiny file does not exist"
         end
-		
-		# Go ahead and read in the first character in the source
-		# code file (if there is one) so that you can begin
-		# lexing the source code file 
-		if (! @f.eof?)
-			@c = @f.getc()
-		else
-			@c = "eof"
-			@f.close()
-		end
-	end
-	
-	# Method nextCh() returns the next character in the file
-	def nextCh()
+        
+        # Go ahead and read in the first character in the source
+        # code file (if there is one) so that you can begin
+        # lexing the source code file 
         if (! @f.eof?)
-			@c = @f.getc()
+            @c = @f.getc()
         else
-			@c = "eof"
-		end
-		
-		return @c
-	end
+            @c = "eof"
+            @f.close()
+        end
+    end
+    
+    # Method nextCh() returns the next character in the file
+    def nextCh()
+        if (! @f.eof?)
+            @c = @f.getc()
+        else
+            @c = "eof"
+        end
+        
+        return @c
+    end
 
-	# Method nextToken() reads characters in the file and returns
-	# the next token
-	# You should also print what you find. Follow the format from the
-	# example given in the instructions.
-	def nextToken() 
+    # Method nextToken() reads characters in the file and returns
+    # the next token
+    # You should also print what you find. Follow the format from the
+    # example given in the instructions.
+    def nextToken() 
         if (@c == "eof")
-			token = Token.new(Token::EOF, "eof")
-				
-		elsif (whitespace?(@c))
-			str = ""
-		
-			while whitespace?(@c)
-				str += @c
-				nextCh()
-			end
-		
-			token = Token.new(Token::WS, str)
+            token = Token.new(Token::EOF, "eof")
+                
+        elsif (whitespace?(@c))
+            str = ""
+        
+            while whitespace?(@c)
+                str += @c
+                nextCh()
+            end
+        
+            token = Token.new(Token::WS, str)
 
         elsif (letter?(@c))
             str = ""
@@ -127,19 +127,19 @@ class Scanner
         puts "Next token is: #{token.type} Next lexeme is: #{token.text}"
         return token
     end
-	
+    
 end
 #
 # Helper methods for Scanner
 #
 def letter?(lookAhead)
-	lookAhead =~ /^([a-z]|[A-Z])$/
+    lookAhead =~ /^([a-z]|[A-Z])$/
 end
 
 def numeric?(lookAhead)
-	lookAhead =~ /^(\d)+$/
+    lookAhead =~ /^(\d)+$/
 end
 
 def whitespace?(lookAhead)
-	lookAhead =~ /^(\s)+$/
+    lookAhead =~ /^(\s)+$/
 end
--- HW-1/TinyToken.rb
+++ HW-1/TinyToken.rb
@@ -5,14 +5,14 @@
 #   @text - the text the token represents (Lexeme)
 #
 class Token
-	attr_accessor :type
-	attr_accessor :text
+    attr_accessor :type
+    attr_accessor :text
 
 # This is the only part of this class that you need to 
 # modify.
-	EOF = "eof"
-	LPAREN = "("
-	RPAREN = ")"
+    EOF = "eof"
+    LPAREN = "("
+    RPAREN = ")"
     ADDOP  = "+"
     SUBOP  = "-"
     MULTOP = "*"
@@ -26,21 +26,21 @@ class Token
 # specified in the Scanner class "TinyScanner.rb"
 
 #constructor
-	def initialize(type,text)
-		@type = type
-		@text = text
-	end
-	
-	def get_type
-		return @type
-	end
-	
-	def get_text
-		return @text
-	end
-	
+    def initialize(type,text)
+        @type = type
+        @text = text
+    end
+    
+    def get_type
+        return @type
+    end
+    
+    def get_text
+        return @text
+    end
+    
 # to string method
-	def to_s
-		return "#{@type} #{@text}"
-	end
+    def to_s
+        return "#{@type} #{@text}"
+    end
 end
